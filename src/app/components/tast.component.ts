4817c09a49d48589855c08d206022d974ae7b38d
NiranjanaNatarajan <niranjana.natarajan@aspiresys.com>
2022-12-29 17:49:52 +0530

    modified stories

---
+++ src/app/components/tast.component.ts
@@ -0,0 +1,18 @@
+
+import { Component, Input, Output, EventEmitter } from '@angular/core';
+
+@Component({
+  selector: 'app-task',
+  templateUrl: './task.component.html',
+})
+export class TaskComponent {
+  @Input() task: any;
+
+  // tslint:disable-next-line: no-output-on-prefix
+  @Output()
+  onPinTask = new EventEmitter<Event>();
+
+  // tslint:disable-next-line: no-output-on-prefix
+  @Output()
+  onArchiveTask = new EventEmitter<Event>();
+}
